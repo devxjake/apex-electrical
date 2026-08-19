dbaf5631fba2b18505cb891b2bea5a8fa3d213a5
Jake Samuels <samuels242@googlemail.com>
2026-08-19 20:34:55 +0100

    Fix compiler error

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    "192.168.0.67",
+    "192.168.0.67:3000",
+    "localhost",
+    "localhost:3000",
+    "127.0.0.1",
+    "127.0.0.1:3000",
+  ],
 };
 
 export default nextConfig;
